ea6327db8993ee45bddb11117669296c6af105c3
Adam Lovastyik <adam@lovastyik.hu>
2019-05-26 19:25:33 +0200

    config screen wip

--- GameOfLife/MainViewController.swift
+++ GameOfLife/MainViewController.swift
@@ -8,9 +8,15 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UITabBarDelegate {
 
     private weak var boardViewController: BoardViewController!
+    
+    @IBOutlet weak var playbackHolderView: UIView!
+    @IBOutlet weak var settingsHolderView: UIView!
+    
+    @IBOutlet weak var mainTabBar: UITabBar!
+    
     
     private var step = 0
     private var stop = false
@@ -30,20 +36,21 @@ class MainViewController: UIViewController {
     
     private func setupUI() {
         
-        title = "Game Of Life"
+        title = NSLocalizedString("Game Of Life", comment: "Main screen title")
         
         addStartButton()
+        setupTabbar()
     }
     
     private func addStartButton() {
         
-        let button = UIBarButtonItem(title: "Start", style: .plain, target: self, action: #selector(startButtonTouched(_:)))
+        let button = UIBarButtonItem(title: NSLocalizedString("Start", comment: "Start buttont title"), style: .plain, target: self, action: #selector(startButtonTouched(_:)))
         navigationItem.rightBarButtonItem = button
     }
     
     private func addStopButton() {
         
-        let button = UIBarButtonItem(title: "Stop", style: .plain, target: self, action: #selector(stopButtonTouched(_:)))
+        let button = UIBarButtonItem(title: NSLocalizedString("Stop", comment: "Stop btton title"), style: .plain, target: self, action: #selector(stopButtonTouched(_:)))
         navigationItem.rightBarButtonItem = button
     }
 
@@ -148,5 +155,45 @@ class MainViewController: UIViewController {
     @objc func stopButtonTouched(_ sender: Any) {
         
         stop = true
+    }
+    
+    // MARK: - Tab bar
+    
+    private func setupTabbar() {
+        
+        mainTabBar.items?.removeAll()
+        
+        let playbackItem = UITabBarItem(title: NSLocalizedString("Playback", comment: "Playback tab bar item title"), image: nil, tag: 0)
+        let setupItem = UITabBarItem(title: NSLocalizedString("Config", comment: "Configuration tab bar item title"), image: nil, tag: 1)
+        
+        mainTabBar.items = [playbackItem, setupItem]
+        mainTabBar.selectedItem = playbackItem
+        
+        updateScreens()
+    }
+    
+    // MARK: Delegate
+    
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        
+        updateScreens()
+    }
+    
+    func updateScreens() {
+    
+        playbackHolderView.isHidden = true
+        settingsHolderView.isHidden = true
+        
+        if let item = mainTabBar.selectedItem {
+            
+            switch item.tag {
+            case 0:
+                playbackHolderView.isHidden = false
+            case 1:
+                settingsHolderView.isHidden = false
+            default:
+                break
+            }
+        }
     }
 }
